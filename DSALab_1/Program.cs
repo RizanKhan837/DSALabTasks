ae7c3e9196242d43f4a6346e40ea1e27fd69a121
rizankhan <rizankhan837@gmail.com>
2022-10-01 23:22:29 +0500

    Added Lab_1

---
+++ DSALab_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DSALabTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+        }
+    }
+}
