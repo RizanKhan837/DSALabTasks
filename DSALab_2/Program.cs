019911770923ca2bd1ef4f3f0ea7ded91eec0f45
rizankhan <rizankhan837@gmail.com>
2022-10-05 07:05:11 +0500

    updated

--- DSALab_2/Program.cs
+++ DSALab_2/Program.cs
@@ -18,12 +18,14 @@ namespace DSALab_2
             selection_Sort(arr);
             displayArray(arr);*/
 
-            /*string[,] chemicals = { {"HCL", "12", "123" },
+            string[,] chemicals = { {"HCL", "12", "123" },
                                     { "CH3", "10", "321" },
                                     { "H2SO4", "24", "541"},
                                     { "NaCl", "23", "544"} };
 
-            int smallest;
+            sortChemicals(chemicals);
+
+            /*int smallest;
             string temp, temp2, temp3;
             int n = chemicals.GetLength(0);
             for (int i = 0; i < n - 1; i++)
@@ -46,9 +48,9 @@ namespace DSALab_2
                 chemicals[i, 1] = temp;
                 chemicals[i, 0] = temp2;
                 chemicals[i, 2] = temp3;
-            }
+            }*/
 
-            Console.WriteLine("Chemicals\tVolume\tConcentration");
+            /*Console.WriteLine("Chemicals\tVolume\tConcentration");
             for (int t = 0; t < chemicals.GetLength(0); t++)
             {
                 for (int j = 0; j < chemicals.GetLength(1); j++)
@@ -59,10 +61,10 @@ namespace DSALab_2
             Console.Write("Input No. Of Products : ");
             int len = Convert.ToInt32(Console.ReadLine());
 
-            string[,] products = new string[2, len];
-
+            string[,] products = new string[len, 2];
 
             input2dArray(products);
+            sortProducts(products);
 
 
             Console.ReadLine();
@@ -189,15 +191,102 @@ namespace DSALab_2
         public static void input2dArray(string[,] arr) {
             for (int i = 0; i < arr.GetLength(0); i++)
             {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    Console.Write("Input Product Name : ");
-                    arr[j, 0] = Console.ReadLine();
-                    Console.Write("Input Price : ");
-                    arr[j, 1] = Console.ReadLine();
-                }
+                Console.Write("Input Product Name : ");
+                arr[i, 0] = Console.ReadLine();
+                Console.Write("Input Price : ");
+                arr[i, 1] = Console.ReadLine();
             }
         }
+        public static void sortChemicals(string[,] chemicals)
+        {
+            int smallest;
+            string temp, temp2, temp3;
+            int n = chemicals.GetLength(0);
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (int.Parse(chemicals[j, 1]) < int.Parse(chemicals[smallest, 1]))
+                        smallest = j;
+                }
+
+                temp = chemicals[smallest, 1];
+                temp2 = chemicals[smallest, 0];
+                temp3 = chemicals[smallest, 2];
+
+                chemicals[smallest, 0] = chemicals[i, 0];
+                chemicals[smallest, 1] = chemicals[i, 1];
+                chemicals[smallest, 2] = chemicals[i, 2];
+
+                chemicals[i, 1] = temp;
+                chemicals[i, 0] = temp2;
+                chemicals[i, 2] = temp3;
+            }
+            
+        }
+        public static void sortProducts(string[,] products)
+        {
+            int smallest;
+            char ans = 'y';
+            string temp, temp2;
+            int n = products.GetLength(0);
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (int.Parse(products[j, 1]) < int.Parse(products[smallest, 1]))
+                        smallest = j;
+                }
+
+                temp = products[smallest, 1];
+                temp2 = products[smallest, 0];
+
+                products[smallest, 0] = products[i, 0];
+                products[smallest, 1] = products[i, 1];
+
+                products[i, 1] = temp;
+                products[i, 0] = temp2;
+            }
+            Console.WriteLine("'\nProduct\t\tPrice");
+            for (int t = 0; t < products.GetLength(0); t++)
+            {
+                for (int j = 0; j < products.GetLength(1); j++)
+                    Console.Write("{0}\t\t", products[t, j]);
+                Console.WriteLine();
+            }
+
+            do
+            {
+                Console.Write("\nEnter Product Name: ");
+                string product = Console.ReadLine();
+                for (int i = 0; i < products.GetLength(0); i++)
+                {
+                    for (int j = 0; j < products.GetLength(1); j++)
+                    {
+                        if (product.Equals(products[j,0]))
+                        {
+                            Console.WriteLine("\tProduct Found!!\n");
+                            Console.WriteLine("Product\tPrice");
+                            Console.Write("{0}\t\t", products[j, 0]);
+                            Console.Write("{0}\t\t", products[j, 1]);
+                            Console.WriteLine("\nDo You Want To Search Another Product [Y/n]");
+                            ans = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                        else {
+                            Console.WriteLine("\tProduct Not Found!!\n");
+                            Console.Write("Do You Want To Search Another Product [Y/n]");
+                            ans = Convert.ToChar(Console.ReadLine());
+                            break;
+                        }
+                    }
+                }
+                
+            } while (ans.Equals('y') || ans.Equals('Y'));
+        }
+
 
     }
 }
