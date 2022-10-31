fbbc467552f33f19cba48a66be71525ae4df68fa
rizankhan <rizankhan837@gmail.com>
2022-10-31 07:29:08 +0500

    Added Lab 6

---
+++ DSALab_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DSALab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", func(123));
+            Console.ReadLine();
+        }
+
+        public static int func(int n) {
+            int sum = 0;
+            while (n >= 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+}
