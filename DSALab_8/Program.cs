e48ebb1cbbb0d6f708217e2a537c0a15e25fd917
rizankhan <rizankhan837@gmail.com>
2022-12-08 18:47:32 +0500

    Lab 8 Updated

--- DSALab_8/Program.cs
+++ DSALab_8/Program.cs
@@ -1,127 +1,85 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace DSALab_8
 {
-    class Program
+    public class BinaryTree
     {
-        internal class Queue
+        static int root = 0;
+        static String[] str = new String[10];
+        /*create root*/
+        public void Root(String key)
         {
-            string[] Messages = new string[5];
-            public ArrayList MessageBuffer = new ArrayList();
-            public int front = -1;
-            public int rear = -1;
-            int i = 0;
-            int msgIndex = 0;
-
-
-            public bool IsEmpty(int front, int rear)
+            str[0] = key;
+        }
+        public void print_Tree()
+        {
+            for (int i = 0; i < 10; i++)
             {
-                if (front == -1 && rear == -1)
-                    return true;
-                else
-                    return false;
-            }
-            public bool IsFull(string[] Messages, int rear)
-            {
-                if (rear == Messages.Length - 1)
-                {
-                    Console.WriteLine("Messages is Full...Delete Some Messages!! \n");
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-
-            public void Enqueue(string message)
-            {
-                if (IsFull(Messages, rear))
-                {
-                    MessageBuffer.Add(message);
-                    return;
-                }
-                else if (IsEmpty(front, rear))
-                {
-                    front = 0;
-                    rear = 0;
-                    Messages[rear] = message;
-
-                }
-                else
-                {
-                    rear = rear + 1;
-                    Messages[rear] = message;
-                }
-            }
-            public void Dequeue()
-            {
-                int index = 0;
-                if (IsEmpty(rear, front))
-                {
-                    return;
-                }
-                else if (front == rear)
-                {
-                    Messages[rear] = MessageBuffer[index].ToString();
-                    MessageBuffer.RemoveAt(index);
-                    front = rear = 0;
-                }
-                else
-                {
-                    //front = front + 1;
-                    for (int i = 0; i < Messages.Length - 1; i++)
-                    {
-                        //string temp = Messages[i];
-                        Messages[i] = Messages[i + 1];
-                        //Messages[i + 1] = temp;
-                    }
-
-                    Messages[rear] = MessageBuffer[index].ToString();
-                    MessageBuffer.RemoveAt(index);
-                    rear = rear + 1;
-                }
-
-            }
-            public void PrintArray()
-            {
-                int count = 1;
-                foreach (var item in Messages)
-                {
-                    Console.WriteLine("Message {0} : {1}", count, item);
-                    count++;
-                }
+                if (str[i] != null)
+                    Console.Write(str[i]);
+                else Console.Write("-");
             }
         }
-        static void Main(string[] args)
+
+
+        /*create right son of root*/
+        public void set_Right(String key, int root)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Queue queue = new Queue();
+            int t = (root * 2) + 2;
+            if (str[root] == null)
+                Console.Write("Can't set child at {0}, no parent found\n", t);
+            else
+                str[t] = key;
+        }
 
-            /*queue.Enqueue("Hello World");
-            queue.Enqueue("Hi..");
-            queue.Enqueue("How");
-            queue.Enqueue("are");
-            queue.Enqueue("You");*/
-
-            queue.Enqueue("Rizan");
-            queue.Enqueue("Khan");
-
-            Console.WriteLine("Messages:");
-            queue.PrintArray();
-            Console.WriteLine();
-
-            Console.WriteLine("Buffer Messages:");
-            foreach (var item in queue.MessageBuffer)
+        /*create left son of root*/
+        public void set_Left(String key, int root)
+        {
+            int t = (root * 2) + 1;
+            if (str[root] == null)
             {
-                Console.WriteLine(item);
+                Console.Write("Can't set child at {0}, no parent found\n", t);
+            }
+            else { str[t] = key; }
+        }
+
+
+        public void preOrder()
+        {
+            Stack<string> nodeStack = new Stack<string>();
+            nodeStack.Push(str[0]);
+
+            while (nodeStack != null)
+            {
+                Console.Write("{0} --> ", nodeStack.Pop());
+
+                int right = (root * 2) + 2;
+                int left = (root * 2) + 1;
+
+                nodeStack.Push(str[right]);
+                nodeStack.Push(str[left]);
+
+                root++;
             }
 
-            queue.Dequeue();
-
-            queue.PrintArray();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree obj = new BinaryTree();
+            obj.Root("A");
+            obj.set_Left("B", 0);
+            obj.set_Right("C", 0);
+            obj.set_Left("D", 1);
+            obj.set_Right("E", 1);
+            obj.set_Left("F", 2);
+            obj.set_Right("G", 2);
+            //obj.print_Tree();
+            obj.preOrder();
 
             Console.ReadLine();
         }
