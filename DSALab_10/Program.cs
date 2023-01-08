3c057d5632af4bba96f1c5e1a233083bc04e5917
rizankhan <rizankhan837@gmail.com>
2023-01-08 16:44:52 +0500

    Lab 9 Complete

---
+++ DSALab_10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSALab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
